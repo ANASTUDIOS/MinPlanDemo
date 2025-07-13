bc4519e640ab2b54e93b073630d4aa142a563468
ANA <aniscratch@gmail.com>
2025-07-13 18:50:59 -0400

    UI lift

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Stack } from "expo-router";
-
+import { Stack} from "expo-router";
 export default function RootLayout() {
   return (
-  <Stack> 
-    <Stack.Screen name="index" />
-  </Stack>
+    <Stack> 
+      <Stack.Screen 
+        name="index" 
+        options={{
+          headerTitle: "Dashboard",
+          headerTitleStyle: {
+            fontFamily: "InstrumentSans_500Medium"
+          },
+          headerTitleAlign: "center"
+      }}
+      />
+      <Stack.Screen 
+        name="about" 
+        options={{
+          headerTitle: "About",
+          headerTitleStyle: {
+            fontFamily: "InstrumentSans_500Medium"
+          },
+          headerTitleAlign: "center"
+        }} 
+      />
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text>Hello Expo</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello PlanningCo</Text>
+      <Link href={"/about"} style={styles.button}>
+        Go to the about screen
+      </Link>
     </View>
   );
 }
@@ -16,5 +18,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#25292e",
-  }
+  },
+  text: {
+    color: "#FFFFFF",
+    fontFamily: "InstrumentSans_400Regular"
+  },
+  button: {
+    fontSize : 20,
+    textDecorationLine: "underline",
+    fontFamily: "InstrumentSans_400Regular",
+    color: "#fff",
+  },
 });
