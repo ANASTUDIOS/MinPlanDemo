91e45b90729d8d7b30c73db9e311f8bb8cb0faad
AnaStudios <anastudios347@gmail.com>
2025-10-23 20:30:32 -0400

    scope shift +improvements

--- app/(tabs)/plan.tsx
+++ app/(tabs)/plan.tsx
@@ -1,10 +1,12 @@
-import { Text, View} from "react-native";
+import DropedDown from "@/components/ThinDropDown";
+import { Text, View } from "react-native";
 import { styles } from "../../styles";
-
 export default function Plan() {
       return (
         <View style={styles.container}>
           <Text style={[styles.header, {paddingLeft:60, paddingRight:60}]}>Use the built-in writting features and AI, to help plan ✒️ your pre-college journey!</Text>
+          <DropedDown name="General Task"/>
         </View>
+
         );
 }
